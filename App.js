d2878c0d42fdcc6f822d15207be8b4121abe929b
Thishan Paramanayagam <jelamable@gmail.com>
2023-01-20 09:15:08 +0530

    Add styles

--- App.js
+++ App.js
@@ -1,20 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start  on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <View style={styles.appcontainer}>
+      <View style={styles.inputcontainer }>
+      <TextInput style={styles.textInput} placeholder="Add My goal"/>
+      <Button title="Add"/>
+      </View>
+      <View>
+      <Text> List of Goals.. </Text>
+      </View>
+      </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  // container: {
+  //   flex: 1,
+  //   backgroundColor: "#fff",
+  //   alignItems: "center",
+  //   justifyContent: "center",
+  // },
+
+  // dtext: {
+  //   margin: 16,
+  //   borderWidth: 2,
+  //    borderColor: "blue",
+  //    padding: 16
+  // }
+appcontainer:{
+  padding:50,
+
+},
+inputcontainer:{
+flexDirection:'row',
+justifyContent: "space-between" 
+},
+textInput:{
+  borderWidth:1,
+  borderColor:'#cccccc',
+  width:'80%'
+}
 });
